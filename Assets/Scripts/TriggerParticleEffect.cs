0e6a3fd7286d3546e76da3f91f816dc9cafb928f
Kylee Robson <11052487@uvu.edu>
2024-11-05 20:41:22 -0700

    Updated Particle Script

--- Assets/Scripts/TriggerParticleEffect.cs
+++ Assets/Scripts/TriggerParticleEffect.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 [RequireComponent(typeof(ParticleSystem), typeof(Collider))]
 public class TriggerParticleEffect : MonoBehaviour
 {
     private ParticleSystem particleSystem; //Reference to the Particle System
-    public int particleAmount = 10; //Expose variable for particle amount
+
+
+    public int firstEmissionAmount = 10; // Exposed variable for first emission
+    public int secondEmissionAmount = 20; //Exposed variable for second emission
+    public int thirdEmissionAmount = 30; //Exposed variable for third emission
+    public float delayBetweenEmissions = 0.5f; //Delay time between emissions
 
 
     private void Start()
@@ -19,7 +25,22 @@ public class TriggerParticleEffect : MonoBehaviour
     {
         if (other.gameObject.GetComponent<CharacterController>()) // Check if the player triggered the event
         {
-            particleSystem.Emit(particleAmount); // Emit the specified amount of particles
+            StartCoroutine(EmitParticlesCoroutine());
         }
+    }
+
+    //Coroutine to emit particles multiple times with delays
+    private IEnumerator EmitParticlesCoroutine()
+    {
+        // First emission
+        particleSystem.Emit(firstEmissionAmount); // Emit based on exposed variable
+        yield return new WaitForSeconds(delayBetweenEmissions); // Wait for specified time
+
+        // Second emission
+        particleSystem.Emit(secondEmissionAmount); // Emit based on exposed variable
+        yield return new WaitForSeconds(delayBetweenEmissions); // Wait for specified time
+
+        // Third emission
+        particleSystem.Emit(thirdEmissionAmount); // Emit based on exposed variable
     }
 }
