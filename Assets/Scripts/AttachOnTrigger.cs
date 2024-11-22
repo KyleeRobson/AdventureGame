661a31b8a95a0f18f77e13910f8605bff2904f22
Kylee Robson <11052487@uvu.edu>
2024-11-21 18:59:52 -0700

    Created Key to turn on Fire

---
+++ Assets/Scripts/AttachOnTrigger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachOnTrigger : MonoBehaviour
+{
+
+    public void OnTriggerEnter(Collider other)
+    {
+        transform.parent = other.transform;
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        transform.parent = null;
+    }
+}
