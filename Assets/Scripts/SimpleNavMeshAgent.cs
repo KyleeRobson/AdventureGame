a21a2862df41f4d6fe8103d7a1440fd49490eb80
Kylee Robson <11052487@uvu.edu>
2024-12-05 22:13:21 -0700

    Added Nav mesh to Frog and Box

---
+++ Assets/Scripts/SimpleNavMeshAgent.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleNavMeshAgent : MonoBehaviour
+{
+    public Transform target; // Assign the TargetPoint in the Inspector
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false; // Disables automatic rotation for 2D
+    }
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+}
